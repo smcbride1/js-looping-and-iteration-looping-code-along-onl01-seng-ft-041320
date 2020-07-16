2f7c5bf1cad592f80cd6dd6e5c577fcaf8fa7722
Stephen McBride <stevemcbride3@gmail.com>
2020-07-16 03:34:32 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-// Code your solutions in this file
+const people = ["Ada", "Brendan", "Ali"]
+const thankYouMessages = []
+
+function writeCards(people, occasion) {
+    for (let i = 0; i < people.length; i++) {
+        thankYouMessages.push(`Thank you, ${people[i]}, for the wonderful ${occasion} gift!`)
+    }
+
+    return thankYouMessages;
+}
+
+function countDown(int) {
+    while (0 <= int) {
+        console.log(int);
+        int--;
+    }
+}
